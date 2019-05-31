893c86b671130266673c36830f9361f399695b81
Thomas Gregory <thomjgregory@gmail.com>
2019-05-31 03:58:12 -0500

    configured build options for Heroku deployment

---
+++ public/precache-manifest.02ce99ac445d97b5e282fea6153e98eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd172fbb2349cd0c594ccacac12d1cd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8927d37020a7f386cb77",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b70f9b122406baf9e091",
+    "url": "/static/js/2.91e21f18.chunk.js"
+  },
+  {
+    "revision": "8927d37020a7f386cb77",
+    "url": "/static/js/main.a76a3084.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
